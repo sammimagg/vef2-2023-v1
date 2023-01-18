9337f59615c0f637e5ec8046219e5c6217bb381e
Samuel Magnusson <sammimagg@vr2t-dw096.rhi.hi.is>
2023-01-18 14:29:20 +0000

    added to script

--- src/build.js
+++ src/build.js
@@ -1,17 +1,48 @@
-import { readFile } from 'fs';
-import {mkdir, writeFile} from 'fs/promises';
-import path, {join} from 'path';
+import { mkdir, writeFile } from 'fs/promises';
+import path, { join } from 'path';
+import { calculate } from './lib/calculator.js';
+import { direxists, readFile, readFilesFromDir } from './lib/file.js';
+import { statsTemplate } from './lib/html.js';
+import { parse } from './lib/parser.js';
 
 const DATA_DIR = './data';
 const OUTPUT_DIR = './dist';
 
 async function main() {
     // ath ef './dist' er til
-    if(!(await direxists(OUTPUT_DIR))){
+    if (!(await direxists(OUTPUT_DIR))) {
         await mkdir(OUTPUT_DIR);
     }
-    
+
     const dataFiles = await readFilesFromDir(DATA_DIR);
     const result = [];
+
+    for (const file of dataFiles) {
+        const content = await readFile(file);
+
+        if (content) {
+            const title = path.basename(file);
+            const numbers = parse(content);
+            const stats = calculate(numbers);
+            const filename = '${title.}.html';
+
+            const result = {
+                title,
+                filename,
+                numbers,
+                stats,
+            };
+            result.push(result);
+
+            const filepath = join(OUTPUT_DIR, filename);
+            const template = statsTemplate(title, result);
+
+            await writeFile(filepath, template, { flag: 'w+' });
+        }
+    }
+
+
+
+
 }
 main().catch((err) => console.error(err));
