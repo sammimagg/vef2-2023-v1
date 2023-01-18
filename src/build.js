7157a77810ed3dd84b14cd4819b7507148f04c27
sammimagg <sammimagg@gmail.com>
2023-01-18 16:44:04 +0000

    prufa

--- src/build.js
+++ src/build.js
@@ -1,6 +1,6 @@
 import { mkdir, writeFile } from 'fs/promises';
 import path, { join } from 'path';
-import { calculate } from './lib/calculator.js';
+//import { calculate } from './lib/calculator.js';
 import { direxists, readFile, readFilesFromDir } from './lib/file.js';
 import { statsTemplate } from './lib/html.js';
 import { parse } from './lib/parser.js';
@@ -15,7 +15,7 @@ async function main() {
     }
 
     const dataFiles = await readFilesFromDir(DATA_DIR);
-    const result = [];
+    const results = [];
 
     for (const file of dataFiles) {
         const content = await readFile(file);
@@ -23,16 +23,15 @@ async function main() {
         if (content) {
             const title = path.basename(file);
             const numbers = parse(content);
-            const stats = calculate(numbers);
+            //const stats = calculate(numbers);
             const filename = '${title.}.html';
 
             const result = {
                 title,
                 filename,
                 numbers,
-                stats,
             };
-            result.push(result);
+            results.push(result);
 
             const filepath = join(OUTPUT_DIR, filename);
             const template = statsTemplate(title, result);
@@ -40,9 +39,9 @@ async function main() {
             await writeFile(filepath, template, { flag: 'w+' });
         }
     }
+    const filepath = join(OUTPUT_DIR, 'index.html');
+    const template = indexTemplate(results);
 
-
-
-
+    await writeFile(filepath, template, { flag: 'w+' });
 }
 main().catch((err) => console.error(err));
